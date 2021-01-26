f91d7afe7594b38284071670254955b17e2c7775
Nicolas <73480455+naubryGV@users.noreply.github.com>
2021-01-26 09:34:25 -0500

    Removing clang-tidy, adding clang-format

--- checkall.sh
+++ checkall.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Let's check if we have a token properly set
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "The GITHUB_TOKEN is required."
 	exit 1
 fi
 
+# Some prerequisites
+wget https://github.com/Sarcasm/run-clang-format/blob/master/run-clang-format.py
+
+# Now let's get the modified files
 echo "Event path: $GITHUB_EVENT_PATH"
 FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
 echo "Files = $FILES_LINK"
@@ -23,50 +28,60 @@ cd files
 for i in "${URLS[@]}"
 do
 	echo "Downloading $i"
-	#curl -u naubryGV:$GITHUB_TOKEN -s $i
-	#echo "Name without web params: ${i%%\?*}"
 	FILENAME=`basename ${i%%\?*}`
 	curl -H "Authorization: token $GITHUB_TOKEN" --header "Accept: application/vnd.github.v3.raw" --header "User-Agent: ${OWNER}/${REPO} (curl v7.47.0)" -L -o $FILENAME $i
-	#--remote-name
 done
 
 echo "Files downloaded!"
 ls -la
 
 echo "Performing checkup:"
-clang-tidy --version
-clang-tidy *.cpp -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* > clang-tidy-report.txt
+# We can't use clang-tidy without checking out the whole repo, or adding a compilation database, so skipping for now
+#clang-tidy --version
+#clang-tidy *.cpp -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* > clang-tidy-report.txt
 
-cppcheck -iclang-format-report.txt -iclang-tidy-report.txt --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *
+ccpcheck --version
+cppcheck -iclang-format-report.txt -iclang-tidy-report.txt --enable=all --std=c++17 --language=c++ --output-file=cppcheck-report.txt *
 
+flawfinder --version
 flawfinder --columns --context --singleline . > flawfinder-report.txt
 
-PAYLOAD_TIDY=`cat clang-tidy-report.txt`
+./../run-clang-format.py --style=file ./* > clang-format-report.txt
+
+#PAYLOAD_TIDY=`cat clang-tidy-report.txt`
 PAYLOAD_CPPCHECK=`cat cppcheck-report.txt`
 PAYLOAD_FLAWFINDER=`cat flawfinder-report.txt`
+PAYLOAD_FORMAT=`cat clang-format-report.txt`
 COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
   
 echo $COMMENTS_URL
-echo "Clang-tidy errors:"
-echo $PAYLOAD_TIDY
+#echo "Clang-tidy errors:"
+#echo $PAYLOAD_TIDY
 echo "Cppcheck errors:"
 echo $PAYLOAD_CPPCHECK
 echo "Flawfinder errors:"
 echo $PAYLOAD_FLAWFINDER
+echo "Clang-format errors:"
+echo $PAYLOAD_FORMAT
 
-OUTPUT=$'**CLANG-TIDY WARNINGS**:\n'
-OUTPUT+=$'\n```\n'
-OUTPUT+="$PAYLOAD_TIDY"
-OUTPUT+=$'\n```\n'
+#OUTPUT=$'**CLANG-TIDY WARNINGS**:\n'
+#OUTPUT+=$'\n```\n'
+#OUTPUT+="$PAYLOAD_TIDY"
+#OUTPUT+=$'\n```\n'
 
-OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
+OUTPUT=$'\n\n**CPPCHECK WARNINGS**:\n'
 OUTPUT+=$'\n```\n'
 OUTPUT+="$PAYLOAD_CPPCHECK"
 OUTPUT+=$'\n```\n' 
 
-OUTPUT+=$'\n**FLAWFINDER WARNINGS**:\n'
+OUTPUT+=$'\n\n**FLAWFINDER WARNINGS**:\n'
 OUTPUT+=$'\n```\n'
 OUTPUT+="$PAYLOAD_FLAWFINDER"
+OUTPUT+=$'\n```\n' 
+
+OUTPUT+=$'\n\n**CLANG-FORMAT WARNINGS**:\n'
+OUTPUT+=$'\n```\n'
+OUTPUT+="$PAYLOAD_FORMAT"
 OUTPUT+=$'\n```\n' 
 
 PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
